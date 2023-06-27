7e9c015e86c339eb3a828873df220145fdde32ee
artemplv <artemplv@gmail.com>
2023-06-27 14:23:28 -0400

    add grid

--- Lecture_Projects/Tic-Tac-Toe/src/index.js
+++ Lecture_Projects/Tic-Tac-Toe/src/index.js
@@ -2,5 +2,10 @@ import Game from '../ttt_node/game';
 import View from './ttt-view';
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Your code here
+  const game = new Game();
+
+  const container = document.querySelector('.ttt');
+
+  const view = new View(game, container);
+  view.setupBoard();
 });
--- Lecture_Projects/Tic-Tac-Toe/src/ttt-view.js
+++ Lecture_Projects/Tic-Tac-Toe/src/ttt-view.js
@@ -1,8 +1,22 @@
 class View {
   constructor(game, el) {
+    this.game = game;
+    this.el = el;
   }
   
   setupBoard() {
+    const ul = document.createElement('ul');
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const li = document.createElement('li');
+        const pos = [i, j];
+        li.dataset.pos = JSON.stringify(pos);
+        ul.appendChild(li);
+      }
+    }
+
+    this.el.appendChild(ul);
   }
   
   handleClick(e) {
